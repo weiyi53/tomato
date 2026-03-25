ea6da019a4b5351ea9a0064e4a7eb5276e530897
weiyi53 <S20233081753@cau.edu.cn>
2026-03-25 20:10:09 +0800

    A

--- yolo/ultralytics/nn/Addmodules/__init__.py
+++
@@ -1,6 +0,0 @@
-from .BiFPN import *
-from .CSPHet import *
-from .C2fCIB import *
-from .PSA import *
-from .SCDown import *
-
--- yolo/ultralytics/nn/DCNv4_op/__init__.py
+++
@@ -1,2 +0,0 @@
-from .DCNv4.functions import DCNv4Function, FlashDeformAttnFunction
-from .DCNv4.modules import DCNv4, FlashDeformAttn
